f6cf2e32db2b29b243a753cc1d463a68a6a4329f
parthraj-mori-1 <parthrajmori2003@gmail.com>
2025-06-14 19:52:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Amplify } from 'aws-amplify'; // 👈 import Amplify
+import { awsConfig } from './aws-config'; // 👈 import your config
 import HomePage from './components/homepage';
 import HomeHealthcare from './pages/HomeReferralPage';
 import InvoiceProcessing from './pages/invoiceprocessing';
 import ChatWithPDF from './pages/Chatpdf';
 import ImmigrationInfo from './pages/immigrationinfo';
-import ProtectedRoute from './components/ProtectedRoute'; // 👈 new import
+import ProtectedRoute from './components/ProtectedRoute';
+
+Amplify.configure(awsConfig); // 👈 configure it once at the top
 
 function App() {
   return (
