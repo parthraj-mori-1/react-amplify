c3c19dc65a4b77b8b562e1de5fd81af6cff9ac3d
parthraj-mori-1 <parthrajmori2003@gmail.com>
2025-06-14 19:33:39 +0530

    Update aws-config.js

---
+++ src/aws-config.js
@@ -0,0 +1,20 @@
+
+import { getCurrentUser, signInWithRedirect } from 'aws-amplify/auth';
+
+
+export const awsConfig = {
+  Auth: {
+    region: "ap-south-1", // e.g. "us-east-1"
+    userPoolId: "ap-south-1_uXraGS772",
+    userPoolWebClientId: "49fn7gpltbgvcrsd33e4mdqfdg",
+    oauth: {
+      domain: "https://ap-south-1uxrags772.auth.ap-south-1.amazoncognito.com",
+      redirectSignIn: "https://main.d1t5v4kvnmyy89.amplifyapp.com/",
+      redirectSignOut: "https://main.d1t5v4kvnmyy89.amplifyapp.com/logout",
+      responseType: "code" // or "code"
+    }
+  }
+};
+
+
+Amplify.configure(awsConfig);
